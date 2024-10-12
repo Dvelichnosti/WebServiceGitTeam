60402c00b5fbc880ed70c00149468ab2cc701298
un1ti <123859521+un1ti@users.noreply.github.com>
2024-10-12 20:29:33 +0300

    v3

---
+++ Hakaton_project/reg/script.js
@@ -0,0 +1,29 @@
+const forms = document.querySelector(".forms"),
+  pwShowHide = document.querySelectorAll(".eye-icon"),
+  links = document.querySelectorAll(".link");
+
+// Add click event listener to each eye icon for toggling password visibility
+pwShowHide.forEach(eyeIcon => {
+  eyeIcon.addEventListener("click", () => {
+    let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+
+    pwFields.forEach(password => {
+      if (password.type === "password") { // If password is hidden
+        password.type = "text"; // Show password
+        eyeIcon.classList.replace("bx-hide", "bx-show"); // Change icon to show state
+        return;
+      }
+      password.type = "password"; // Hide password
+      eyeIcon.classList.replace("bx-show", "bx-hide"); // Change icon to hide state
+    });
+
+  });
+});
+
+// Add click event listener to each link to toggle between forms
+links.forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault(); // Prevent default link behavior
+    forms.classList.toggle("show-signup");
+  });
+});
